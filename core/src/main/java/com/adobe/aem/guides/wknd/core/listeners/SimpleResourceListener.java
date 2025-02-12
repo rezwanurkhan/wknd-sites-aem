b4e8aa8a509ef4f340779bc16990be9d81ab0ad9
KHANMR <mdrezwanurrahman.khan@abbvie.com>
2025-02-12 14:21:07 +0600

    test commit with build failure

--- core/src/main/java/com/adobe/aem/guides/wknd/core/listeners/SimpleResourceListener.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/listeners/SimpleResourceListener.java
@@ -40,7 +40,7 @@ public class SimpleResourceListener implements ResourceChangeListener {
 
     @Override
     public void onChange(List<ResourceChange> changes) {
-        changes.forEach(change -> {
+        changes.forEach(change ->
             logger.debug("Resource event: {} at: {} isExternal", change.getType(), change.getPath(), change.isExternal());
         });
         
